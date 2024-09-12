ea2c92ffadc311f4f3f9f65e634da030dfcb09c4
Harshit Shukla <harshit.shukla005@gmail.com>
2024-09-12 15:49:48 +0530

    3rd project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import GamePlay from "./components/GamePlay";
+import StartGame from "./components/StartGame";
+
+function App() {
+
+  const [isGameStarted,setIsGameStarted] = useState(false); //usestate aisa hook hai jisse humlog state manage kar pate hain
+
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev)=>!prev);
+  };
+
+  return (
+    <>
+      {
+        isGameStarted ? <GamePlay/> : <StartGame toggle = {toggleGamePlay} />
+      }
+    </>
+  );
+}
+
+export default App;
+
